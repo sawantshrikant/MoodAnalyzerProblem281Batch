68b664be2dc7f7dc540556d23d3af22db3377cc8
Shrikant <sawantshrikant07@gmail.com>
2023-07-03 19:48:22 +0530

    [Shrikant] add:WelcomeMeassage

---
+++ MoodAnalyzerProblem281Batch/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerProblem281Batch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Mood Analyser Problem 281Batch!");
+        }
+    }
+}
